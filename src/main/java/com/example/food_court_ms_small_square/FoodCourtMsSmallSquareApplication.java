4391baea8ea99b4f64e31ae25ff9b3ea71e6b786
SayiraQuirama <sayira.quirama@pragma.com.co>
2025-02-21 16:57:45 -0500

    Initial configuration

---
+++ src/main/java/com/example/food_court_ms_small_square/FoodCourtMsSmallSquareApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_court_ms_small_square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCourtMsSmallSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCourtMsSmallSquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/food_court_ms_small_square/FoodCourtMsSmallSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_court_ms_small_square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCourtMsSmallSquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
